14f2a12e696d2afbd991b212a384b44792a2233e
Sayan Pramanick <sayan.pramanick@qubrid.com>
2024-09-22 19:51:41 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -9,10 +9,16 @@ const port = process.env.PORT || 8000;
 // middlewares
 app.use(express.json());
 app.use(cors());
-const mongoURI = "mongodb+srv://sayanpramanick:afYxTzznf9zpcShx@expensemanagementcluste.08es3.mongodb.net/"
+
+// const mongoURI = "mongodb+srv://sayanpramanick:afYxTzznf9zpcShx@expensemanagementcluste.08es3.mongodb.net/"
+const mongoURI = "mongodb+srv://sayanpramanick:afYxTzznf9zpcShx@expensemanagementcluste.08es3.mongodb.net/test?retryWrites=true&w=majority&appName=ExpenseManagementCluster"
 mongoose.connect(mongoURI)
     .then(() => console.log("Connected to mongoDB!"))
     .catch(err => console.error("Connection failed mongoDB:", err))
+
+
+app.get("/", (req, res) => { res.send("Hello from expense tracker server !") })
+
 app.use("/", allRoutes)
 
 
